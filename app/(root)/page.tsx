64179ee822ae6e7475105242efa9fb2558c40b58
iamtayyab007 <tayyabm336@gmail.com>
2025-05-10 00:32:18 +0500

    all files

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,18 @@
 import React from "react";
+import HeaderBox from "../components/HeaderBox";
 
 export default function Home() {
-  return <div>page</div>;
+  const loggedIn = { firstName: "Tayyab" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <HeaderBox
+          type="greeting"
+          title="Welcome"
+          user={loggedIn?.firstName || "Guest"}
+          subtext="Access and manage your account and transactions efficiently"
+        />
+      </div>
+    </section>
+  );
 }
